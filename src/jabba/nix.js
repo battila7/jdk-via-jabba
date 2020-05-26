e930a93ea6e7fe8fcefabd6864affae332b3ec87
Attila Bagossy <bagossyattila@outlook.com>
2020-05-26 20:53:03 +0200

    Applied linter fixes

--- src/jabba/nix.js
+++ src/jabba/nix.js
@@ -1,18 +1,18 @@
-async function installJabba() {
-    const jabbaInstallerPath = await this._deps.cache.downloadTool('https://github.com/shyiko/jabba/raw/master/install.sh')
+async function installJabba () {
+  const jabbaInstallerPath = await this._deps.cache.downloadTool('https://github.com/shyiko/jabba/raw/master/install.sh')
 
-    await this._deps.exec.exec('bash', [jabbaInstallerPath])
+  await this._deps.exec.exec('bash', [jabbaInstallerPath])
 }
 
-function jabbaPath() {
-    const homeDirectory = this._deps.process.env.HOME
+function jabbaPath () {
+  const homeDirectory = this._deps.process.env.HOME
 
-    return this._deps.path.join(homeDirectory, '.jabba', 'bin', 'jabba')
+  return this._deps.path.join(homeDirectory, '.jabba', 'bin', 'jabba')
 }
 
 const nixImpl = {
-    installJabba,
-    jabbaPath
+  installJabba,
+  jabbaPath
 }
 
 module.exports = nixImpl
