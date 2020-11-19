de1783776e8cba5495fdb036da5e34ab57b909a3
Attila Bagossy <bagossyattila@outlook.com>
2020-11-19 22:34:15 +0100

    feat: added the Windows implementation of installing jabba

--- dist/index.js
+++ dist/index.js
@@ -5096,9 +5096,42 @@ module.exports = nixImpl
 /***/ }),
 
 /***/ 941:
-/***/ (() => {
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-throw new Error('Windows support is not implemented yet!')
+const { spawnSync } = __webpack_require__(129)
+
+const installerUri = 'https://github.com/shyiko/jabba/raw/master/install.ps1'
+
+const installerScript = `
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-Expression (
+  Invoke-WebRequest ${installerUri} -UseBasicParsing
+).Content
+`.trim()
+
+async function installJabba () {
+  const { error } = spawnSync('powershell', {
+    stdio: 'inherit',
+    input: installerScript
+  })
+
+  if (error) {
+    throw error
+  }
+}
+
+function jabbaPath () {
+  const homeDirectory = this._deps.process.env.HOME
+
+  return this._deps.path.join(homeDirectory, '.jabba', 'bin', 'jabba')
+}
+
+const windowsImpl = {
+  installJabba,
+  jabbaPath
+}
+
+module.exports = windowsImpl
 
 
 /***/ }),
--- src/jabba/windows.js
+++ src/jabba/windows.js
@@ -1,1 +1,34 @@
-throw new Error('Windows support is not implemented yet!')
+const { spawnSync } = require('child_process')
+
+const installerUri = 'https://github.com/shyiko/jabba/raw/master/install.ps1'
+
+const installerScript = `
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-Expression (
+  Invoke-WebRequest ${installerUri} -UseBasicParsing
+).Content
+`.trim()
+
+async function installJabba () {
+  const { error } = spawnSync('powershell', {
+    stdio: 'inherit',
+    input: installerScript
+  })
+
+  if (error) {
+    throw error
+  }
+}
+
+function jabbaPath () {
+  const homeDirectory = this._deps.process.env.HOME
+
+  return this._deps.path.join(homeDirectory, '.jabba', 'bin', 'jabba')
+}
+
+const windowsImpl = {
+  installJabba,
+  jabbaPath
+}
+
+module.exports = windowsImpl
